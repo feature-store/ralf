a81366a1518365a0fdf6a88ec92dde2b8117b2d6
Sarah Wooders <sarahwooders@gmail.com>
2021-10-05 20:25:56 -0700

    Add JSON source (#4)

--- ralf/simulation/source.py
+++ ralf/simulation/source.py
@@ -1,3 +1,5 @@
+import json
+from collections import defaultdict
 from typing import List, Optional
 
 import pandas as pd
@@ -54,4 +56,58 @@ class Source:
                         )
                     )
             record_id += 1
+            yield self.env.timeout(1 / (self.records_per_sec_per_key))
+
+
+class JSONSource:
+    def __init__(
+        self,
+        env: simpy.Environment,
+        records_per_sec_per_key: int,
+        num_keys: int,
+        next_queue: simpy.Store,
+        total_run_time: Optional[float] = None,
+        data_file: Optional[str] = None,
+    ):
+        self.env = env
+        self.records_per_sec_per_key = records_per_sec_per_key
+        self.num_keys = num_keys
+        self.next_queue = next_queue
+        self.total_run_time = total_run_time
+        self.data = json.load(open(data_file))
+        self.index = 0
+        self.optimal_plan = defaultdict(list)
+
+        self.env.process(self.run())
+
+    def run(self):
+        record_id = 0
+        while True:
+            if (
+                self.index >= len(self.data)
+                or self.total_run_time
+                and self.env.now > self.total_run_time
+            ):
+                print(
+                    f"Complete source ts: {self.index}/{len(self.data)}, env time: {self.env.now}/{self.total_run_time}"
+                )
+                open("optimal_plan.json", "w").write(json.dumps(self.optimal_plan))
+                break
+
+            # send for each key at timestep
+            for key in self.data[self.index].keys():
+                edit = self.data[self.index][key]  # list of diff.json files
+                # print(f"Send {self.index}, {self.env.now}: {edit}")
+                self.optimal_plan[self.env.now] += [(e, key) for e in edit]
+                yield self.next_queue.put(
+                    Record(
+                        key=key,  # doc_id
+                        value=edit,
+                        seq_id=record_id,
+                        processing_time=self.env.now,
+                    )
+                )
+
+            record_id += 1
+            self.index += 1
             yield self.env.timeout(1 / (self.records_per_sec_per_key))
