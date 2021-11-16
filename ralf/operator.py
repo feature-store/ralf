31def39f5de07937ba41259a278bf9e5e702181b
David Chu <davidchuyaya@gmail.com>
2021-11-15 20:01:19 -0800

    added delete_record()

--- ralf/operator.py
+++ ralf/operator.py
@@ -204,6 +204,10 @@ class Operator(ABC):
                 event.process()
 
     @abstractmethod
+    def delete_record(self, record: Record):
+        pass
+
+    @abstractmethod
     def on_record(self, record: Record) -> Optional[Record]:
         pass
 
