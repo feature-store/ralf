60a66cfd3339a79671f911b094121fb95dbdf53c
simon-mo <simon.mo@hey.com>
2022-02-09 00:51:24 -0800

    revert unintentional

--- ralf/state.py
+++ ralf/state.py
@@ -5,26 +5,26 @@ from typing import Any, Dict, Type
 
 
 class Record:
-    def __init__(self, **entry: Dict[str, Any]):
-        self.entry: Dict[str, Any] = entry
+    def __init__(self, **entries: Dict[str, Any]):
+        self.entries: Dict[str, Any] = entries
         self._source = None
         self.processing_time = time.time()
 
-        for k, v in entry.items():
+        for k, v in entries.items():
             setattr(self, k, v)
 
     # def __str__(self):
-    #     return str(self.entry)
+    #     return str(self.entries)
 
     def __repr__(self):
-        return f"Record({self.entry},processing_time={self.processing_time:.2f})"
+        return f"Record({self.entries},processing_time={self.processing_time:.2f})"
 
     def __eq__(self, other: "Record") -> bool:
-        return self.entry == other.entry and self._source == other._source
+        return self.entries == other.entries and self._source == other._source
 
     # TODO: Uhh this makes ray not work
     # def __dict__(self):
-    #    return self.entry
+    #    return self.entries
 
 
 class Schema:
@@ -36,7 +36,7 @@ class Schema:
     def validate_record(self, record: Record):
         # TODO: add type checking.
         schema_columns = set(self.columns.keys()).union(set([self.primary_key]))
-        record_columns = set(record.entry.keys())
+        record_columns = set(record.entries.keys())
         assert (
             schema_columns == record_columns
         ), f"schema columns are {schema_columns} but record has {record_columns}"
