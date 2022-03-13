11ae3411c1cca5ecaa60fa4e0801f4ac9829686c
Sarah Wooders <sarahwooders@gmail.com>
2022-03-12 16:14:49 -0800

    add simple counter example

---
+++ ralf/v2/examples/counter.py
@@ -0,0 +1,81 @@
+from ralf.v2 import LIFO, FIFO, BaseTransform, RalfApplication, RalfConfig, Record, BaseScheduler
+from ralf.v2.operator import OperatorConfig, SimpyOperatorConfig, RayOperatorConfig
+import time
+
+from typing import List
+from collections import defaultdict
+from dataclasses import dataclass
+
+
+@dataclass
+class SourceValue:
+    key: str
+    value: int
+    timestamp: float
+
+@dataclass
+class SumValue:
+    key: str
+    value: int  
+
+class FakeSource(BaseTransform):
+    def __init__(self, total: int) -> None:
+        self.count = 0
+        self.total = total
+        self.num_keys = 10
+
+    def on_event(self, _: Record) -> List[Record[SourceValue]]:
+
+        if self.count >= self.total: 
+            print("completed iteration")
+            raise StopIteration()
+
+        self.count += 1
+        key = str(self.count % self.num_keys)
+
+        # sleep to slow down send rate
+        time.sleep(1)
+
+        # return list of records (wrapped around dataclass)
+        return [
+            Record(
+                entry=SourceValue(
+                    key=key,
+                    value=self.count, 
+                    timestamp=time.time(), 
+                ), 
+                shard_key=key # key to shard/query 
+            )
+        ]
+
+class Sum(BaseTransform):
+
+    def __init__(self): 
+        self.total = defaultdict(lambda: 0)
+
+    def on_event(self, record: Record) -> Record[SumValue]:
+        self.total[record.entry.key] += record.entry.value
+        print(f"Record {record.entry.key}, value {self.total[record.entry.key]}")
+        return Record(entry=SumValue(key=record.entry.key, value=self.total[record.entry.key]))
+
+if __name__ == "__main__":
+
+    deploy_mode = "local"
+    app = RalfApplication(RalfConfig(deploy_mode=deploy_mode))
+
+    source_ff = app.source(
+        FakeSource(10000),
+        operator_config=OperatorConfig(
+            ray_config=RayOperatorConfig(num_replicas=1),
+        ),
+    )
+
+    sum_ff = source_ff.transform(
+        Sum(),
+        operator_config=OperatorConfig(
+            ray_config=RayOperatorConfig(num_replicas=1),
+        ),
+    )
+
+    app.deploy()
+    app.wait()
