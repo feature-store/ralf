92f0c32a4b61a07582ee8a83e7f1ff8d6980433c
Jeffery Cheng <jeffcheng1234@berkeley.edu>
2022-04-09 04:11:31 -0700

    added table state and connector class, table state to feature frame (not sure what that really does though, it still gets passed into feature frame)

--- ralf/v2/examples/counter.py
+++ ralf/v2/examples/counter.py
@@ -2,6 +2,9 @@ import time
 from collections import defaultdict
 from dataclasses import dataclass
 from typing import List
+from ralf.v2.dict_connector import DictConnector
+from ralf.v2.record import Schema
+from ralf.v2.table_state import TableState
 
 from ralf.v2 import BaseTransform, RalfApplication, RalfConfig, Record
 from ralf.v2.operator import OperatorConfig, RayOperatorConfig
@@ -53,14 +56,44 @@ class FakeSource(BaseTransform):
 
 class Sum(BaseTransform):
     def __init__(self):
-        self.total = defaultdict(lambda: 0)
+        self.total = 0
 
     def on_event(self, record: Record) -> Record[SumValue]:
-        self.total[record.entry.key] += record.entry.value
-        print(f"Record {record.entry.key}, value {self.total[record.entry.key]}")
+        self.total += record.entry.value
+        print(f"Record {record.entry.key}, value {str(self.total)}")
         return Record(
-            entry=SumValue(key=record.entry.key, value=self.total[record.entry.key])
+            entry=SumValue(key=record.entry.key, value=self.total)
         )
+
+class UpdateDict(BaseTransform):
+    def __init__(self, table_state: TableState):
+        self.table_state = table_state
+
+    def on_event(self, record: Record) -> None:
+        print("single update table", self.table_state.connector.tables)
+        self.table_state.update(record)
+        return None
+    
+class BatchUpdate(BaseTransform):
+    def __init__(self, table_state:TableState ,batch_size: int):
+        self.batch_size = batch_size
+        self.count = 0
+        self.records = []
+        self.table_state = table_state
+
+    def on_event(self, record: Record) -> None:
+        self.records.append(record)
+        self.count += 1
+        
+        if self.count >= self.batch_size:
+            self.count = 0
+            for r in self.records:
+                print(f"batch update, processing {r}")
+                self.table_state.update(r)
+            self.records = []
+            print("batch table", self.table_state.connector.tables)
+        
+        return None
 
 
 if __name__ == "__main__":
@@ -70,7 +103,7 @@ if __name__ == "__main__":
     app = RalfApplication(RalfConfig(deploy_mode=deploy_mode))
 
     source_ff = app.source(
-        FakeSource(10000),
+        FakeSource(10),
         operator_config=OperatorConfig(
             ray_config=RayOperatorConfig(num_replicas=1),
         ),
@@ -78,6 +111,29 @@ if __name__ == "__main__":
 
     sum_ff = source_ff.transform(
         Sum(),
+        operator_config=OperatorConfig(
+            ray_config=RayOperatorConfig(num_replicas=1),
+        ),
+    )
+
+    dict_schema = Schema("key", {"key": str, "value": int})
+    dict_schema_1 = Schema("key", {"key": str, "value": int})
+
+    dict_conn = DictConnector()
+    dict_conn_1 = DictConnector()
+
+    dict_table_state = TableState(dict_schema, dict_conn)
+    batch_table_state = TableState(dict_schema_1, dict_conn_1)
+
+    update_ff = sum_ff.transform(
+        UpdateDict(dict_table_state),
+        operator_config=OperatorConfig(
+            ray_config=RayOperatorConfig(num_replicas=1),
+        ),
+    )
+
+    batch_update_ff = sum_ff.transform(
+        BatchUpdate(batch_table_state, 3),
         operator_config=OperatorConfig(
             ray_config=RayOperatorConfig(num_replicas=1),
         ),
